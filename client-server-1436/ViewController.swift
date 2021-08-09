7f073940bf390b55834d586744f5359900513686
stoicist <art.igberdin@gmail.com>
2021-08-09 20:47:23 +0300

    Initialize project

---
+++ client-server-1436/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  client-server-1436
+//
+//  Created by Artur Igberdin on 09.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
